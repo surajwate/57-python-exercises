4d53919e5946331b1c50078b0ca2afefdc85d580
Suraj Wate <suraj.wate@gmail.com>
2021-07-06 23:20:15 +0530

    made few changes in exercise01.py

--- exercises/exercise01.py
+++ exercises/exercise01.py
@@ -1,6 +1,8 @@
-def hello(name):
-        return (f'Hello {name}, nice to meet you!')
-        
+
+def hello(greet_name):
+    return f'Hello {greet_name}, nice to meet you!'
+
+
 if __name__ == '__main__':
     name = input("What is your name?")
     print(hello(name))
