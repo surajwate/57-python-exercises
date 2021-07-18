3b47c096266fec114300b9340cd852088a00d56e
Suraj Wate <suraj.wate@gmail.com>
2021-07-18 23:50:35 +0530

    refactor exercise 21

--- exercises/exercise21.py
+++ exercises/exercise21.py
@@ -9,6 +9,23 @@ def number_to_name(number):
     return ("The name of the month is " + mapped[number-1] + ".")
 
 
+def get_month():
+    ans = True
+    while ans:
+        try:
+            mn = int(input("Please enter the number of the month: "))
+
+        except ValueError:
+            print("Please enter numberical value between and including 1 and 12.")
+
+        finally:
+            if(mn >= 1 and mn <= 12):
+                return mn
+            elif(mn <= 1 or mn >= 12):
+                print("Please enter value between and including 1 and 12.")
+                ans = True
+
+
 if __name__ == '__main__':
-    mn = int(input("Please enter the number of the month: "))
+    mn = get_month()
     print(number_to_name(mn))
--- tests/test_exercise21.py
+++ tests/test_exercise21.py
@@ -5,3 +5,22 @@ import pytest
 @pytest.mark.parametrize("number, month", [(1, "The name of the month is January."), (7, "The name of the month is July."), (11, "The name of the month is November.")])
 def test_number_to_name(number, month):
     assert number_to_name(number) == month
+
+
+""" 
+
+def test_wrong_number():
+    try:
+        number_to_name(13)
+    except KeyError as e:
+        assert e
+    else:
+        raise AssertionError("expected to raise KeyError")
+
+     """    
+
+@pytest.mark.parametrize('num', [-1, 13, -15, 25])
+def test_wrong_number(num):
+    with pytest.raises(KeyError):
+        number_to_name(num)
+
